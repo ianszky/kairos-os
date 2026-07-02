8dcf7a8a6f3103bd883e526b63c14d0d1d7cf547
Ian Agustin <ian.agustin071729@gmail.com>
2026-07-03 00:34:09 +0800

    feat: Perfect 1:1 UI implementation of mindful-launcher-7.html in Jetpack Compose

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
 
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-compiler:2.50")
